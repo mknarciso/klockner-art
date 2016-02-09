b40c9cf5631250af407a2a31827eff574b8bd84f
mknarciso <murilokn@gmail.com>
2016-02-09 23:41:20 +0000

    Refix Loja e AWS

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'coffee-rails', '~> 4.1.0'
 
 # Image Handling
 gem 'paperclip', '~> 4.3', '>= 4.3.2'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,15 @@ Rails.application.configure do
   
   Rails.application.routes.default_url_options[:host] = 'klockner-art.herokuapp.com'
   
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
